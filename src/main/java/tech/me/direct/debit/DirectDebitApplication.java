612054db85bbaf7a822e17c8e6a02c731104f6ea
Aref Parhizkari <arefpin1@gmail.com>
2025-03-11 03:43:36 +0330

    init(Project): created project

---
+++ src/main/java/tech/me/direct/debit/DirectDebitApplication.java
@@ -0,0 +1,13 @@
+package tech.me.direct.debit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectDebitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirectDebitApplication.class, args);
+    }
+
+}
