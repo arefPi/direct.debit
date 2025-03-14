8b9aaa12021354467f12ee2d7f4a6303bd8131a9
Aref Parhizkari <arefpin1@gmail.com>
2025-03-14 12:52:01 +0330

    refactor(Service): Refactored MandateAuthorizationCodeRequestUriBuilder to accept more additional query param

--- src/main/java/tech/me/direct/debit/service/mandate/redirect/uri/builder/MandateAuthorizationCodeRequestUriBuilder.java
+++ src/main/java/tech/me/direct/debit/service/mandate/redirect/uri/builder/MandateAuthorizationCodeRequestUriBuilder.java
@@ -2,24 +2,31 @@ package tech.me.direct.debit.service.mandate.redirect.uri.builder;
 
 import lombok.Builder;
 import org.springframework.web.util.UriComponentsBuilder;
+import java.util.HashMap;
+import java.util.Map;
+
 @Builder
 public class MandateAuthorizationCodeRequestUriBuilder implements AuthorizationCodeRequestUriBuilder {
     private final String providerAddress;
     private final String clientId;
     private final String redirectUri;
     private final String state;
-    private final String mandateReferenceId;
+    @Builder.Default
+    private final Map<String, String> additionalParams = new HashMap<>();
 
     @Override
     public String toUriString() {
-        return UriComponentsBuilder
+        UriComponentsBuilder builder = UriComponentsBuilder
                 .fromUriString(providerAddress)
                 .queryParam("response_type", "code")
                 .queryParam("redirect_uri", redirectUri)
                 .queryParam("client_id", clientId)
                 .queryParam("scope", "mandate")
-                .queryParam("state", state)
-                .queryParam("mandate_reference_id", mandateReferenceId)
+                .queryParam("state", state);
+
+        additionalParams.forEach(builder::queryParam);
+
+        return builder
                 .build()
                 .encode()
                 .toUriString();
