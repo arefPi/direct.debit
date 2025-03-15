e307636e1c3f6cde9fdb9e3d41d820c642cd1b30
Aref Parhizkari <arefpin1@gmail.com>
2025-03-15 17:42:57 +0330

    refactor(config): Moved BeanConfig

--- src/main/java/tech/me/direct/debit/config/BeanConfig.java
+++ src/main/java/tech/me/direct/debit/config/BeanConfig.java
@@ -1,4 +1,4 @@
-package tech.me.direct.debit.config.properties;
+package tech.me.direct.debit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
